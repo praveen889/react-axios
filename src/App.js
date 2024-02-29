203c4417b9a772869413fa90c024c916e7acee98
Praveen <60999279+praveen889@users.noreply.github.com>
2024-02-29 20:01:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+//{ npx create-react-app name-of-your-app } It will create the blank React App.
+// { npm install axios } This will install the axios module in the react project. 
+//-----    { WITH THE HELP OF THE AXIOS WE CAN CALL THE API }    -----//
+//-- In this project i call the localhost:3000/api  --// 
+//-- {This Backend Api is in (backend-node-js-express) Repository.} --//
+
+//Import axios to the project.
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
@@ -5,20 +12,27 @@ import axios from 'axios';
 
 
 function App() {
+  //{ useState } This function is used to set the state of the  function.
   const [data, setData] = useState([]);
 
   useEffect(()=>{
     fetchData();
+
   },[]);
   const fetchData = async() =>{
+    //In this try and catch block we are try to get the data from the localhost server.
+
     try{
       const res = await axios.get('http://localhost:3000/api/data');
       setData(res.data);
-    }catch(err){
+    }
+    //If there is an issue to connect with the localhost server it will pass the "Error" with the exact error message. 
+    catch(err){
       console.log("Error",err)
     }
   }
   
+// In this return block destructure the the array with whole data present in the array.
 
   return (
     <div className='app'>
